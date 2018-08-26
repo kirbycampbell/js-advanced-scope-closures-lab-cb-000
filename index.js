3c7942376b2e834bfa39e91df002264464576ef8
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-26 22:25:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function produceDrivingRange(blockRange){
   return function(blockOne, blockTwo) {
-    return true ;
+    let distance = Math.abs(parseInt(blockOne) - parseInt(blockTwo));
+    let diff = Math.abs(blockRange - distance);
+
+    if (distance > blockRange) {
+      return `${diff}blocks out of range`;
+    }else {
+      `within range by ${diff}`
+    }
     }
 
   }
