d7a4ffb350ba88f05dc6eb4eef70b2b4eec089c4
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-26 22:16:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function produceDrivingRange(distance){
-  return function() {
-    if distance > 10 ? return true : return false
+function produceDrivingRange(range){
+  return function(multiplier) {
+    return range * multiplier;
     }
 
   }
